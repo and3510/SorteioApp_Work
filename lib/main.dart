3f36fea1b5d62c4d71d4e20959782a8f03d212fe
anderson <and.dovale@gmail.com>
2025-03-11 19:44:59 -0300

    conectar front e backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,111 @@
+// import 'dart:math';
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         elevatedButtonTheme: ElevatedButtonThemeData(
+//           style: ElevatedButton.styleFrom(
+//             foregroundColor: Colors.white, backgroundColor: const Color.fromARGB(255, 61, 124, 82),     // cor do texto
+//             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+//             shape: RoundedRectangleBorder(
+//               borderRadius: BorderRadius.circular(10),
+//           )
+//         ),
+//       ),
+//       ),
+//       home: const MyHomePage(title: 'Sorteio.somosicev'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+ 
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+
+//   void _incrementCounter() {
+//     setState(() {
+      
+//       _counter++;
+//     });
+//   }
+
+//   List<int> numeros = [0,1,2,3,4,5,6,7,8,9,10];
+
+//   int sortear() {
+//     Random random = Random();
+//     int numeroSorteado = random.nextInt(numeros.length);
+//     return numeros[numeroSorteado];
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+   
+//     return Scaffold(
+//       appBar: AppBar(
+        
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+       
+//         title: Text(widget.title), centerTitle: true,
+//       ),
+//       body: Container(
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text(
+//                 'Texto em cima',
+//                 style: TextStyle(
+//                   fontSize: 30,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.red, // Altere conforme necessário
+//                 ),
+//               ),
+//               const SizedBox(height: 50),
+
+//               ElevatedButton(
+//                 onPressed: (){
+//                   setState(() {
+                    
+//                   });
+//                 }, 
+//                 child: const Text("Sortear"),
+//                 style: ElevatedButton.styleFrom(
+//                     padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20), // Aumenta o espaçamento interno
+//                     minimumSize: Size(200, 50),
+//               ),
+//             ),
+//             ],
+//           ),
+//         ),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
+
+
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,7 +115,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,13 +122,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.white, backgroundColor: const Color.fromARGB(255, 61, 124, 82),     // cor do texto
+            foregroundColor: Colors.white, backgroundColor: const Color.fromARGB(255, 61, 124, 82),
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
-          )
+            ),
+          ),
         ),
-      ),
       ),
       home: const MyHomePage(title: 'Sorteio.somosicev'),
     );
@@ -31,8 +138,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
- 
-
   final String title;
 
   @override
@@ -40,23 +145,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _numeroSorteado = 0;
 
-  void _incrementCounter() {
+  List<int> numeros = [0,1,2,3,4,5,6,7,8,9,10];
+
+  void sortear() {
+    Random random = Random();
+    int numeroSorteado = random.nextInt(numeros.length);
     setState(() {
-      
-      _counter++;
+      _numeroSorteado = numeros[numeroSorteado];
     });
   }
 
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
         title: Text(widget.title), centerTitle: true,
       ),
       body: Container(
@@ -69,27 +174,30 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
-                  color: Colors.red, // Altere conforme necessário
+                  color: Colors.red,
                 ),
               ),
               const SizedBox(height: 50),
-
+              Text(
+                'Número sorteado: $_numeroSorteado',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 50),
               ElevatedButton(
-                onPressed: (){
-                  setState(() {
-                    
-                  });
-                }, 
+                onPressed: sortear,
                 child: const Text("Sortear"),
                 style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20), // Aumenta o espaçamento interno
-                    minimumSize: Size(200, 50),
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  minimumSize: Size(200, 50),
+                ),
               ),
-            ),
             ],
           ),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
